db1ca2363ab31441791403cb48e5b80859bf4965
Patrick Herbst <p.herbst.1987@gmail.com>
2024-03-10 10:42:23 +0100

    get request to weather api with coordinates for Hamburg, DE

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ import bodyParser from "body-parser";
 const port = 3000;
 const app = express();
 
+
+const API_url = "https://api.openweathermap.org/data/3.0/onecall?";
+
+const hamburgLat = 53.5502;
+const hamburgLon = 9.9920;
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    const result = await axios.get(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${hamburgLat}&lon=${hamburgLon}&appid=${apiKey}`);
+    console.log(result.data);
+    res.render("index.ejs", { content: result.data });
 });
 
 app.listen(port, () => {
